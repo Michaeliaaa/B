4d5e7f2ce91ea1b799efc2c4b6f400530ff587d7
Michaelia Tan <Michaelia0816@me.com>
2022-09-13 21:37:50 +0800

    Link frontend and backend together

--- frontend/src/components/reviews-list.component.js
+++ frontend/src/components/reviews-list.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import ReviewDataService from "../services/review.service";
-import { Link } from "react-router-dom";
 
 export default class ReviewsList extends Component {
   constructor(props) {
