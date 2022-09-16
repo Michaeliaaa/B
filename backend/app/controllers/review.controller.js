49f3902ef20ddb1dec8d34cd9a0778c2f28848bf
Michaelia Tan <Michaelia0816@me.com>
2022-09-16 20:57:26 +0800

    Add more validations

--- backend/app/controllers/review.controller.js
+++ backend/app/controllers/review.controller.js
@@ -5,7 +5,74 @@ const Review = db.reviews;
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.module) {
-    res.status(400).send({ message: "Content can not be empty!" });
+    res.status(400).send({ message: "Module code cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.ay) {
+    res.status(400).send({ message: "Academic year cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.semester) {
+    res.status(400).send({ message: "Academic semester cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.description) {
+    res.status(400).send({ message: "Description cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.expected) {
+    res.status(400).send({ message: "Expected grade cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.grade) {
+    res.status(400).send({ message: "Actual grade cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.rating) {
+    res.status(400).send({ message: "Rating cannot be empty!" });
+    return;
+  }
+
+  if (!req.body.submitter) {
+    res.status(400).send({ message: "Submitter cannot be empty! Please provide a nickname." });
+    return;
+  }
+
+  if (!(/^\d{2}\/\d{2}$/).test(req.body.ay)) {
+    res.status(400).send({ message: "Academic year must be in the format of xx/xx, e.g. 22/23" });
+    return;
+  }
+
+  if (!(req.body.semester == "Sem 1" || req.body.semester == "Sem 2" || req.body.semester == "ST I" || req.body.semester == "ST II")) {
+    res.status(400).send({ message: "Invalid semester. It should be one of the following: Sem 1/Sem 2/ST I/ST II" });
+  }
+  
+  if (!(req.body.expected == "A+" || req.body.expected == "A" || req.body.expected == "A-" ||
+    req.body.expected == "B+" || req.body.expected == "B" || req.body.expected == "B-" ||
+    req.body.expected == "C+" || req.body.expected == "C" ||
+    req.body.expected == "D+" || req.body.expected == "D" ||
+    req.body.expected == "F")) {
+    res.status(400).send({ message: `${req.body.expected} is an invalid letter grade. It should be one of the following: A+/A/A-/B+/B/B-/C+/C/D+/D/F` });
+    return;
+  }
+
+  if (!(req.body.grade == "A+" || req.body.grade == "A" || req.body.grade == "A-" ||
+    req.body.grade == "B+" || req.body.grade == "B" || req.body.grade == "B-" ||
+    req.body.grade == "C+" || req.body.grade == "C" ||
+    req.body.grade == "D+" || req.body.grade == "D" ||
+    req.body.grade == "F")) {
+    res.status(400).send({ message: `${req.body.grade} is an invalid letter grade. It should be one of the following: A+/A/A-/B+/B/B-/C+/C/D+/D/F` });
+    return;
+  }
+
+  if (!(/^[1-5]$/).test(req.body.rating)) {
+    res.status(400).send({ message: "Rating must be a number between 1 and 5." });
     return;
   }
 
